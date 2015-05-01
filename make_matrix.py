999d14512819d93d371d0e9f7b0a06ca04575191
Rathijeet Bhave <rathijeetbhave@gmail.com>
2015-05-01 21:57:58 +0530

    no message

--- make_matrix.py
+++ make_matrix.py
@@ -2,7 +2,7 @@ import csv
 import itertools
 import sys
 def get_stud_row():
-    with open('std_aoe_4.txt','r') as f:
+    with open('stud_aoi_real.csv','r') as f:
         output = csv.reader(f,delimiter = ' ')
         for row in output:
             yield row
@@ -10,7 +10,7 @@ def get_stud_row():
 
 def get_aoe_comm_list():
     aoe_comm_list = []
-    with open('aoe_comm_2.txt','r') as f:
+    with open('aoi_comm_real.csv','r') as f:
         output = csv.reader(f,delimiter = ' ')
         for line in output:
             aoe_comm_list.append(line)
@@ -18,7 +18,8 @@ def get_aoe_comm_list():
 
 
 
-def some_name(alpha,max_count):
+def some_name(alpha,max_count,gamma):
+    #test_graph = 200 - gamma*int(row[0])
     alf = int(alpha)
     list_alpha,list_one_minus_alpha = segregate_edges()
     aoe_comm_dict,avg_outdegree = find_avg_outdegree()
@@ -26,18 +27,20 @@ def some_name(alpha,max_count):
     aoe_comm_list = get_aoe_comm_list()
     pref_graph=[300,200,100]
     for row in get_stud_row():
+        test_graph = 20 - gamma*int(row[0])/10
+        #test_graph = 1
         for char in row[1:]:
-            for elem in aoe_comm_list[int(char)-1][1:]:
+            for elem in aoe_comm_list[int(char[0])-1][1:]:
                 for i in range(1,len(pref_graph)+1):
                     if row[i] == char :
                         if (row[0],elem) in list_alpha:                          
-                            if  matrix[int(row[0])-1][(int(elem)-1)*max_count] < pref_graph[i-1]*alf*(len(aoe_comm_list[int(char)-1])-1):
+                            if  matrix[int(row[0])-1][(int(elem[0])-1)*max_count] < test_graph*pref_graph[i-1]*alf*(len(aoe_comm_list[int(char)-1])-1):
                                 for j in range(max_count):
-                                    matrix[int(row[0])-1][(int(elem)-1)*max_count + j] = pref_graph[i-1]*alf*(len(aoe_comm_list[int(char)-1])-1)
+                                    matrix[int(row[0])-1][(int(elem[0])-1)*max_count + j] = test_graph*pref_graph[i-1]*alf*(len(aoe_comm_list[int(char)-1])-1)
                         else:
-                            if matrix[int(row[0])-1][(int(elem)-1)*max_count] < pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[int(char)-1])-1):
+                            if matrix[int(row[0])-1][(int(elem[0])-1)*max_count] < test_graph*pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[int(char[0])-1])-1):
                                 for j in range(max_count):
-                                    matrix[int(row[0])-1][(int(elem)-1)*max_count + j] = pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[int(char)-1])-1)
+                                    matrix[int(row[0])-1][(int(elem[0])-1)*max_count + j] = test_graph*pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[int(char[0])-1])-1)
 
 
 
@@ -52,7 +55,7 @@ def find_avg_outdegree():
     total_committees = 0
     aoe_comm_dict = {}
     _sum = 0
-    with open('aoe_comm_2.txt','r') as f:
+    with open('aoi_comm_real.csv','r') as f:
         output = csv.reader(f,delimiter = ' ')
         for row in output:
             aoe_comm_dict[row[0]] = int(len(row[1:]))
@@ -61,6 +64,7 @@ def find_avg_outdegree():
         _sum += i
     avg_outdegree = _sum/len(aoe_comm_dict)
     return (aoe_comm_dict,avg_outdegree)
+    #print aoe_comm_dict,avg_outdegree
 
 
 
@@ -68,15 +72,20 @@ def segregate_edges():
     aoe_comm_dict,avg_outdegree = find_avg_outdegree()
     list_alpha = []
     list_one_minus_alpha = []
-    with open('std_aoe_4.txt','r') as f:
+    with open('stud_aoi_real.csv','r') as f:
         output = csv.reader(f,delimiter = ' ')
         for row in output:
             for num in row[1:]:
-                if aoe_comm_dict[num]>avg_outdegree:
-                    list_one_minus_alpha.append((row[0],num))
-                else:
-                    list_alpha.append((row[0],num))
+                try:
+                    if aoe_comm_dict[num]>avg_outdegree:
+                        list_one_minus_alpha.append((row[0],num))
+                    else:
+                        list_alpha.append((row[0],num))
+                except Exception,e:
+                    print str(e)
     return (list_alpha,list_one_minus_alpha)
+    #print list_alpha,list_one_minus_alpha
+    #print list_one_minus_alpha
 
 
 
@@ -94,7 +103,7 @@ def make_matrix():
 
 
 if __name__ == '__main__':
-    some_name(sys.argv[1],sys.argv[2])
-    #make_matrix()
+    #some_name(sys.argv[1],sys.argv[2])
+    make_matrix()
     #get_stud_row()
     #get_aoe_comm_list()
--- munkres_test.py
+++ munkres_test.py
@@ -1,13 +1,14 @@
 from munkres import Munkres,print_matrix
 import make_matrix
 import sys
+import itertools
 
-def munkres_test(alpha,max_count):
+def munkres_test(alpha,max_count,gamma):
     m=Munkres()
     #matrix=[[5,9,1],[10,3,2],[8,7,4]]
     #matrix=[[100,102,1,1],[100,102,1,1],[200,200,202,202],[200,200,202,202]]
     #matrix=[[20,160,1,1],[1,80,40,1],[20,1,1,160],[1,80,40,1]]
-    matrix = make_matrix.some_name(alpha,int(max_count))
+    matrix = make_matrix.some_name(alpha,int(max_count),int(gamma))
     cost_matrix = []
     for row in matrix:
         cost_row = []
@@ -15,14 +16,25 @@ def munkres_test(alpha,max_count):
                 cost_row += [sys.maxsize - col]
         cost_matrix += [cost_row]
     indexes=m.compute(cost_matrix)
-    print_matrix(matrix, msg='Lowest cost through this matrix:')
+    print indexes
+
+    #for row,tuples in itertools.izip(make_matrix.get_stud_row(),indexes):
+        ##print row,tuples
+        #print row.index(tuples[
+        
+    #print_matrix(matrix, msg='Lowest cost through this matrix:')
     total = 0
     for row, column in indexes:
             value = matrix[row][column]
             total += value
             print '(%d, %d) -> %d' % (row, column, value)
+
+        
+
     print 'total cost: %d' % total
 
 
+
+
 if __name__ == '__main__':
-    munkres_test(sys.argv[1],sys.argv[2])
+    munkres_test(sys.argv[1],sys.argv[2],sys.argv[3])
