8a632dd559c31c61ee83dbd7333bacfa880fc06b
Rathijeet Bhave <rathijeetbhave@gmail.com>
2015-05-06 23:26:50 +0530

    Working file for real data of year 2014

--- make_matrix.py
+++ make_matrix.py
@@ -2,7 +2,7 @@ import csv
 import itertools
 import sys
 def get_stud_row():
-    with open('stud_aoi_real_1.csv','r') as f:
+    with open('stud_aoi_real.csv','r') as f:
         output = csv.reader(f,delimiter = ' ')
         for row in output:
             yield row
@@ -18,29 +18,49 @@ def get_aoe_comm_list():
 
 
 
+#def some_name(alpha,max_count,gamma):
 def some_name(alpha,max_count,gamma):
     #test_graph = 200 - gamma*int(row[0])
     alf = int(alpha)
+    gamma = int(gamma) 
+    print alf,max_count,gamma
+    #max_count = 4
+    no_of_committees = 15
+    no_of_students = 150
     list_alpha,list_one_minus_alpha = segregate_edges()
     aoe_comm_dict,avg_outdegree = find_avg_outdegree()
-    matrix = [[1]*max_count*len(aoe_comm_dict) for _ in range(max_count*len(aoe_comm_dict))]
+    #print len(aoe_comm_dict)
+    #matrix = [[1]*max_count*len(aoe_comm_dict) for _ in range(max_count*len(aoe_comm_dict))]
+    matrix = [[1]*no_of_committees*max_count for _ in range(no_of_students)]
     aoe_comm_list = get_aoe_comm_list()
-    pref_graph=[300,200,100]
+    #print aoe_comm_list
+    #pref_graph=[300,200,100,50]
+    pref_graph=[500,400,100,10]
     for row in get_stud_row():
         test_graph = 20 - gamma*int(row[0])/10
         #test_graph = 1
-        for char in row[1:]:
-            for elem in aoe_comm_list[int(char[0])-1][1:]:
-                for i in range(1,len(pref_graph)+1):
-                    if row[i] == char :
-                        if (row[0],elem) in list_alpha:                          
-                            if  matrix[int(row[0])-1][(int(elem[0])-1)*max_count] < test_graph*pref_graph[i-1]*alf*(len(aoe_comm_list[int(char)-1])-1):
-                                for j in range(max_count):
-                                    matrix[int(row[0])-1][(int(elem[0])-1)*max_count + j] = test_graph*pref_graph[i-1]*alf*(len(aoe_comm_list[int(char)-1])-1)
-                        else:
-                            if matrix[int(row[0])-1][(int(elem[0])-1)*max_count] < test_graph*pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[int(char[0])-1])-1):
-                                for j in range(max_count):
-                                    matrix[int(row[0])-1][(int(elem[0])-1)*max_count + j] = test_graph*pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[int(char[0])-1])-1)
+        for aoe in row[1:]:
+            index_in_comm_list = find_aoe_comm_index(aoe_comm_list,aoe)
+            try:
+                for comm in aoe_comm_list[index_in_comm_list][1:]:
+                    #print aoe,comm
+                    #char = aoe
+                    #comm = elem
+                    for i in range(1,len(pref_graph)+1):
+                        if row[i] == aoe :
+                            if (row[0],aoe) in list_alpha:                          
+                                if  matrix[int(row[0])-1][(int(comm)-1)*max_count] < test_graph*pref_graph[i-1]*alf*(len(aoe_comm_list[index_in_comm_list])-1):
+                                    for j in range(max_count):
+                                        matrix[int(row[0])-1][(int(comm)-1)*max_count + j] = test_graph*pref_graph[i-1]*alf*(len(aoe_comm_list[index_in_comm_list])-1)
+                                break
+                            else:
+                                if matrix[int(row[0])-1][(int(comm)-1)*max_count] < test_graph*pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[index_in_comm_list])-1):
+                                    for j in range(max_count):
+                                        matrix[int(row[0])-1][(int(comm)-1)*max_count + j] = test_graph*pref_graph[i-1]*(10-alf)*(len(aoe_comm_list[index_in_comm_list])-1)
+                                break
+            except TypeError:
+                #print str(e)
+                pass
 
 
 
@@ -64,7 +84,7 @@ def find_avg_outdegree():
         _sum += i
     avg_outdegree = _sum/len(aoe_comm_dict)
     return (aoe_comm_dict,avg_outdegree)
-    #print aoe_comm_dict,avg_outdegree
+    #print aoe_comm_dict,avg_outdegree,len(aoe_comm_dict)
 
 
 
@@ -72,7 +92,7 @@ def segregate_edges():
     aoe_comm_dict,avg_outdegree = find_avg_outdegree()
     list_alpha = []
     list_one_minus_alpha = []
-    with open('stud_aoi_real_1.csv','r') as f:
+    with open('stud_aoi_real.csv','r') as f:
         output = csv.reader(f,delimiter = ' ')
         for row in output:
             for num in row[1:]:
@@ -81,18 +101,28 @@ def segregate_edges():
                         list_one_minus_alpha.append((row[0],num))
                     else:
                         list_alpha.append((row[0],num))
-                except Exception,e:
-                    print str(e)
+                except KeyError:
+                    #print str(e)
+                    pass
+    #print list_alpha
     return (list_alpha,list_one_minus_alpha)
     #print list_alpha,list_one_minus_alpha
     #print list_one_minus_alpha
+
+
+def find_aoe_comm_index(aoe_comm_list,elem):
+    for i,lst in enumerate(aoe_comm_list):
+        if elem == lst[0]:
+            return i
+
+
 
 
 
 
 
 def make_matrix():
-    some_name(alpha,max_count)
+    some_name(alpha,max_count,gamma)
     #find_avg_outdegree()
     #segregate_edges()
 
@@ -103,7 +133,7 @@ def make_matrix():
 
 
 if __name__ == '__main__':
-    #some_name(sys.argv[1],sys.argv[2])
+    #some_name(sys.argv[1],sys.argv[2],sys.argv[3])
     make_matrix()
     #get_stud_row()
     #get_aoe_comm_list()
